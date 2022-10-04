eef9a571fb44a53e08d61501003b82ccb33824e0
Luckiest7 <luckiest7luckiest@gmail.com>
2022-10-05 04:19:16 +0800

    first

---
+++ src/Sample6.java
@@ -0,0 +1,8 @@
+public class Sample6 {
+    public static void main(String[] args){
+        System.out.println("十進位數10的是"+10);
+        System.out.println("八進位數10的是"+010);
+        System.out.println("十六進位數10的是"+0x10);
+        System.out.println("十六進位數F的是"+0xF);
+    }
+}
